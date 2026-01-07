d6c9c69a56a8255b7e4cf5414bb9014c6c945090
An Huy <anhuypc147@gmail.com>
2026-01-07 16:41:12 +0700

    first commit for backend

---
+++ istar-backend/src/main/java/com/haui/istar/IstarApplication.java
@@ -0,0 +1,13 @@
+package com.haui.istar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstarApplication.class, args);
+	}
+
+}
---
+++ istar-backend/src/test/java/com/haui/istar/IstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haui.istar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
